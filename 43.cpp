45b8f9a51988b03b0e35ec46a920edad62d99830
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-11-02 08:59:12 +0700

    Multiply Strings

---
+++ 43.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if (num1 == "0" || num2 == "0") return "0";
+
+        vector<int> res(num1.size() + num2.size(), 0);
+
+        for (int i = num1.size() - 1; i >= 0; i--) {
+            for (int j = num2.size() - 1; j >= 0; j--) {
+                res[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+                res[i + j] += res[i + j + 1] / 10;
+                res[i + j + 1] %= 10;
+            }
+        } 
+
+        int i = 0;
+        string ans;
+
+        // loại bỏ số 0 đầu tiên
+        while (res[i] == 0) i++;
+
+        while (i < res.size()) ans += to_string(res[i++]);
+        return ans;
+    }
+};
+
+int main () {
+    string n1, n2;
+    cin >> n1 >> n2;
+
+    Solution sol;
+    cout << sol.multiply(n1, n2);
+}
