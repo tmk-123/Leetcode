f73c0b13a2beb5c8cfa13b3179d2f74e338cc4c5
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-11-06 18:37:53 +0700

    Word Search

--- 79.cpp
+++ 79.cpp
@@ -9,29 +9,33 @@ int n, m;
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
+        n = board.size();
+        m = board[0].size();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (word[0] == board[i][j]) {
                     vector<vector<bool>> visited(n, vector<bool>(m, false));
                     visited[i][j] = true;
-                    if (DFS(board, word, i, j, 1, visited)) return true;
+                    if (DFS(board, word, i, j, 1, visited, n, m)) return true;
                 }
             }
         }
         return false;
     }
 
-    int DFS(vector<vector<char>>& board, string word, int x, int y, int cnt, vector<vector<bool>> &visited) {
+    int DFS(vector<vector<char>>& board, string word, int x, int y, int cnt, vector<vector<bool>> &visited, int n, int m) {
         if (cnt == word.length()) {
             return 1;
         }
+
         for (int i = 0; i < 4; i++) {
             int newX = x + dx[i];
             int newY = y + dy[i];
 
             if (newX >= 0 && newX < n && newY >= 0 && newY < m && word[cnt] == board[newX][newY] && !visited[newX][newY]) {
                 visited[newX][newY] = true;
-                if(DFS(board, word, newX, newY, cnt + 1, visited)) return 1;
+
+                if(DFS(board, word, newX, newY, cnt + 1, visited, n, m)) return 1;
                 visited[newX][newY] = false;
             }
         }
