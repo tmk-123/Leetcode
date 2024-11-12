58a2c6c2868f69470bd52886ef207079d3323444
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-11-12 23:00:52 +0700

    Minimum Path Sum

---
+++ 64.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i > 0 && j > 0) dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                else if (i > 0) dp[i][j] = dp[i - 1][j] + grid[i][j];
+                else if (j > 0) dp[i][j] = dp[i][j - 1] + grid[i][j];
+            }
+        }
+        
+        return dp[n - 1][m - 1];
+    }
+};
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> v(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> v[i][j];
+        }
+    }
+
+    Solution sol;
+    cout << sol.minPathSum(v);
+}
