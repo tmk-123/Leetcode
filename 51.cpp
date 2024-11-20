4131682d399e4a526c376b7bb4b7ce0a1b98c713
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-11-20 15:32:22 +0700

    N-Queens

--- 51.cpp
+++ 51.cpp
@@ -4,19 +4,17 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<string>> res;
-
     vector<vector<string>> solveNQueens(int n) {
         vector<string> board(n, string(n, '.'));
         // diagonal1: đường chéo phụ, diag2 đường chéo chính(đi từ trên trái xuống dưới phải)
         vector<bool> cols(n, false), diag1(2 * n - 1, false), diag2(2 * n - 1, false);
-
-        solve(0, board, n, cols, diag1, diag2);
+        vector<vector<string>> res;
+        solve(0, board, n, cols, diag1, diag2, res);
 
         return res;
     }
 
-    void solve(int row, vector<string>& board, int n, vector<bool>& cols, vector<bool>& diag1, vector<bool>& diag2) {
+    void solve(int row, vector<string>& board, int n, vector<bool>& cols, vector<bool>& diag1, vector<bool>& diag2, vector<vector<string>>& res) {
         if (row == n) {
             res.push_back(board);
             return;
@@ -28,7 +26,7 @@ public:
                 board[row][col] = 'Q';
                 cols[col] = diag1[row + col] = diag2[row - col + n - 1] = true;
 
-                solve(row + 1, board, n, cols, diag1, diag2);
+                solve(row + 1, board, n, cols, diag1, diag2, res);
 
                 board[row][col] = '.';
                 cols[col] = diag1[row + col] = diag2[row - col + n - 1] = false;
