dbcd86f4038157d894e9d9fc70c3afd4c736796d
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-11-24 10:22:43 +0700

    Valid Palindrome II

---
+++ 680.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+    class Solution {
+    public:
+        bool isPalindrome(const string& s, int left, int right) {
+            while (left < right) {
+                if (s[left] != s[right]) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        bool validPalindrome(string s) {
+            int left = 0, right = s.length() - 1;
+            while (left < right) {
+                if (s[left] != s[right]) {
+                    return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+    };
+
+int main() {
+    string s;
+    cin >> s;
+    Solution sol;
+    cout << sol.validPalindrome(s);
+}
