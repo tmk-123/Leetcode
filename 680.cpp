57e2cb44530c2aa9b3125f7728d5ddd12bc2dbfd
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-11-24 10:23:34 +0700

    Valid Palindrome II

--- 680.cpp
+++ 680.cpp
@@ -2,32 +2,31 @@
 
 using namespace std;
 
-    class Solution {
-    public:
-        bool isPalindrome(const string& s, int left, int right) {
-            while (left < right) {
-                if (s[left] != s[right]) {
-                    return false;
-                }
-                left++;
-                right--;
+class Solution {
+public:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
             }
-            return true;
+            left++;
+            right--;
         }
+        return true;
+    }
 
-        bool validPalindrome(string s) {
-            int left = 0, right = s.length() - 1;
-            while (left < right) {
-                if (s[left] != s[right]) {
-                    return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
-                }
-                left++;
-                right--;
+    bool validPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
             }
-            return true;
+            left++;
+            right--;
         }
-
-    };
+        return true;
+    }
+};
 
 int main() {
     string s;
