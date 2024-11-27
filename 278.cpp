3012d31b6a5e772fbff43d4d22e0260148e2d94f
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-11-27 22:29:50 +0700

    First Bad Version

---
+++ 278.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (isBadVersion(mid)) right = mid;
+            else left = mid + 1;
+        }
+
+        return left;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    Solution sol;
+    cout << sol.firstBadVersion(n);
+}
