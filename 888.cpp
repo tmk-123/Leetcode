0913501dff83dada947c0be41e3dbe6a70356601
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-11-28 16:17:00 +0700

    Fair Candy Swap

--- 888.cpp
+++ 888.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+
+using namespace std;
 
 class Solution {
 public:
@@ -19,3 +22,13 @@ public:
         return {};
     }
 };
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) cin >> b[i];
+    Solution sol;
+    sol.fairCandySwap(a, b);
+}
