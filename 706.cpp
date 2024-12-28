fb905ca6b5e99ff2048ed8863b28983f7af63d48
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-12-28 18:13:36 +0700

    Design HashMap

---
+++ 706.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class MyHashMap {
+private:
+    vector<int> hashMap;
+public:
+    MyHashMap() {
+        hashMap.resize(1000001, -1);
+    }
+    
+    void put(int key, int value) {
+        hashMap[key] = value;
+    }
+    
+    int get(int key) {
+        return hashMap[key];
+    }
+    
+    void remove(int key) {
+        hashMap[key] = -1;
+    }
+};
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
