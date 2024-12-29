3995f78e90891fc742f41226835a9e367af7180d
Tran Manh Khiem <khiemmanhtran@gmail.com>
2024-12-29 15:43:46 +0700

    Swap Nodes in Pairs

---
+++ 24.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {} 
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* temp = head;
+
+        while (temp && temp->next) {
+            swap(temp->val, temp->next->val);
+            temp = temp->next->next;
+        }
+
+        return head;
+    }
+};
+
+void insertAtTail(ListNode*& head, ListNode*& tail, int value) {
+    ListNode* newNode = new ListNode(value);
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next; 
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        insertAtTail(head, tail, value);
+    }
+
+    Solution sol;
+    ListNode* res = sol.swapPairs(head);
+    printList(res);
+}
