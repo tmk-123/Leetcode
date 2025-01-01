c2c464724d994166775a4d86629b56eac6df556a
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-01 07:03:37 +0700

    Reorder List

---
+++ 143.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {} 
+};
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* prev = nullptr;
+        ListNode* cur = slow->next;
+        slow->next = nullptr;
+        while (cur) {
+            ListNode* temp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = temp;
+        }
+
+        ListNode* first = head;
+        ListNode* second = prev;
+        // lặp đến second null, nếu theo first thì có thể thừa
+        while (second) {
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+
+            first->next = second;
+            second->next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
+
+void insertAtTail(ListNode*& head, ListNode*& tail, int value) {
+    ListNode* newNode = new ListNode(value);
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next; 
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        insertAtTail(head, tail, value);
+    }
+
+    Solution sol;
+    sol.reorderList(head);
+}
