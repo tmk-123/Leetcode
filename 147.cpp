fe03f3429196cadd7b71718e88b5302434eb9706
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-01 07:33:03 +0700

    Insertion Sort List

---
+++ 147.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {} 
+};
+
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = head;
+        while (cur) {
+            ListNode* nextNode = cur->next;
+            ListNode* prev = dummy;
+
+            while (prev->next && prev->next->val < cur->val) {
+                prev = prev->next;
+            }
+
+            cur->next = prev->next;
+            prev->next = cur;
+
+            cur = nextNode;
+        }
+
+        return dummy->next;
+    }
+};
+
+void insertAtTail(ListNode*& head, ListNode*& tail, int value) {
+    ListNode* newNode = new ListNode(value);
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next; 
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        insertAtTail(head, tail, value);
+    }
+
+    Solution sol;
+    ListNode* res = sol.insertionSortList(head);
+    printList(res);
+}
