b0db6b7ee2863e5fa9c6b300b734f9f585da919d
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-02 00:14:09 +0700

    Delete the Middle Node of a Linked List

---
+++ 2095.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {} 
+};
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (!head || !head->next) return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        slow->next = slow->next->next;
+        return head;
+    }
+};
+
+void insertAtTail(ListNode*& head, ListNode*& tail, int value) {
+    ListNode* newNode = new ListNode(value);
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next; 
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    ListNode* head = nullptr;
+    ListNode* tail = nullptr;
+    for (int i = 0; i < n; i++) {
+        int value;
+        cin >> value;
+        insertAtTail(head, tail, value);
+    }
+
+    Solution sol;
+    ListNode* res = sol.deleteMiddle(head);
+    printList(res);
+}
