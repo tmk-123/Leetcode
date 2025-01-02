a112eb62356642f056b2f75fc3eafc2ba96d57cd
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-02 22:14:35 +0700

    Delete Node in a Linked List

---
+++ 237.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {} 
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+       node->val = node->next->val;
+       node->next = node->next->next; 
+    }
+};
+void insertAtTail(ListNode*& head, ListNode*& tail, int value) {
+    ListNode* newNode = new ListNode(value);
+
+    if (head == nullptr) {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next; 
+    }
+}
