1cea2ac2315422d60c7a32f2147a5bb5dbc152cf
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-06 15:09:53 +0700

    Cousins in Binary Tree

--- 993.cpp
+++ 993.cpp
@@ -39,3 +39,37 @@ public:
         return (depthX == depthY) && (parentX != parentY);
     }
 };  
+TreeNode* createTree(vector<int> nodes) {
+    if (nodes.empty()) return nullptr;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    // hàng đợi để duyệt cùng độ sâu
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* cur = q.front();
+        q.pop();
+
+        if (nodes[i] != -1) {
+            cur->left = new TreeNode(nodes[i]);
+            q.push(cur->left);
+        }
+        i++;
+
+        if (i < nodes.size() && nodes[i] != -1) {
+            cur->right = new TreeNode(nodes[i]);
+            q.push(cur->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+int main() {
+    vector<int> nodes1 = {1, 2, 3, -1, 4, -1, 5};
+    TreeNode* root1 = createTree(nodes1);
+    Solution sol;
+    cout << sol.isCousins(root1, 4, 5);
+}
