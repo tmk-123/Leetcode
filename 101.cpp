43f4e05708f557fcbdb3a6d4cae776504e502bfd
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-07 15:30:35 +0700

    Symmetric Tree

---
+++ 101.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool check(TreeNode* root1, TreeNode* root2) {
+        if (!root1 && !root2) return true;
+        if (!root1 || !root2) return false;
+        if (root1->val != root2->val) return false;
+        return (check(root1->left, root2->right) && check(root1->right, root2->left)); 
+    }
+    bool isSymmetric(TreeNode* root) {
+        return check(root->left, root->right);        
+    }
+};
+int main() {
+    TreeNode* root1 = new TreeNode(1);
+    root1->left = new TreeNode(2);
+    root1->right = new TreeNode(2);
+    root1->left->left = new TreeNode(3);
+    root1->left->right = new TreeNode(4);
+    root1->right->left = new TreeNode(4);
+    root1->right->right = new TreeNode(3);
+
+    Solution sol;
+    cout << sol.isSymmetric(root1) << endl;
+
+    return 0;
+}
