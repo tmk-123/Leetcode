82dcf8afd4d39552dc777ac152f306b29997d081
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-07 18:19:08 +0700

    Merge Two Binary Trees

---
+++ 617.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if (!root1) return root2;
+        if (!root2) return root1;
+
+        TreeNode* merged = new TreeNode(root1->val + root2->val);
+        merged->left = mergeTrees(root1->left, root2->left);
+        merged->right = mergeTrees(root1->right, root2->right);
+
+        return merged;
+    }
+};
+
+void inorder(TreeNode* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+int main() {
+    TreeNode* root1 = new TreeNode(1);
+    root1->left = new TreeNode(3);
+    root1->right = new TreeNode(2);
+    root1->left->left = new TreeNode(5);
+
+    TreeNode* root2 = new TreeNode(2);
+    root2->left = new TreeNode(1);
+    root2->right = new TreeNode(3);
+    root2->left->right = new TreeNode(4);
+    root2->right->right = new TreeNode(7);
+
+    Solution sol;
+    TreeNode* res = sol.mergeTrees(root1, root2);
+    inorder(res);
+
+    return 0;
+}
