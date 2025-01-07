1ae80d789f6c16169ad0c327cc53d0800a7324d6
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-07 15:17:21 +0700

    Same Tree

---
+++ 100.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+
+        if (p->val != q->val) return false;
+        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+};
+int main() {
+    TreeNode* root1 = new TreeNode(1);
+    root1->left = new TreeNode(2);
+    root1->right = new TreeNode(3);
+    root1->left->left = new TreeNode(4);
+    root1->left->right = new TreeNode(5);
+
+    TreeNode* root2 = new TreeNode(1);
+    root2->left = new TreeNode(2);
+    root2->right = new TreeNode(3);
+    root2->left->left = new TreeNode(4);
+    root2->left->right = new TreeNode(7);
+
+    Solution sol;
+    cout << sol.isSameTree(root1, root2) << endl;
+
+    return 0;
+}
