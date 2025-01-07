9c091faaf280e5ce68cbdd100128ee9a11523360
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-07 15:11:05 +0700

    Binary Tree Inorder Traversal

---
+++ 94.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void inorder(TreeNode* node, vector<int>& res) {
+        if (!node) return;
+
+        inorder(node->left, res);
+        res.push_back(node->val);
+        inorder(node->right, res);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        inorder(root, res);
+        return res;
+    }
+};
+int main() {
+    TreeNode* root1 = new TreeNode(1);
+    root1->left = new TreeNode(2);
+    root1->right = new TreeNode(3);
+    root1->left->left = new TreeNode(4);
+    root1->left->right = new TreeNode(5);
+
+    Solution sol;
+    vector<int> res = sol.inorderTraversal(root1);
+    for (auto x : res) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
