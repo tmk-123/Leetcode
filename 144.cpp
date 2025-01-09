ed4fda7c55037c72f8328e25ac38dcad633c46c0
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-09 18:46:07 +0700

    Binary Tree Preorder Traversal

---
+++ 144.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        preorder(root, res);
+        return res;
+    }
+    void preorder(TreeNode* root, vector<int>& res) {
+        if (!root) return;
+        res.push_back(root->val);
+        preorder(root->left, res);
+        preorder(root->right, res);
+    }
+};
+void inorder(TreeNode* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+int main() {
+    TreeNode* root1 = new TreeNode(5);
+    root1->left = new TreeNode(3);
+    root1->right = new TreeNode(6);
+    root1->left->left = new TreeNode(2);
+    root1->left->right = new TreeNode(4);
+    root1->right->right = new TreeNode(7);
+
+    Solution sol;
+    vector<int> res = sol.preorderTraversal(root1);
+    for (int x : res) {
+        cout << x << " ";
+    }
+}   
