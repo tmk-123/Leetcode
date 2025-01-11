7edb6b61cb29f24d2ad2b2e5c440bd9161527115
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-11 12:07:31 +0700

    Root Equals Sum of Children

---
+++ 2236.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool checkTree(TreeNode* root) {
+        if (root->val == root->left->val + root->right->val) return true;
+        return false;
+    }
+};
+void inorder(TreeNode* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->val << " ";
+    inorder(root->right);
+}
+int main() {
+    TreeNode* root1 = new TreeNode(4);
+    root1->left = new TreeNode(2);
+    root1->right = new TreeNode(7);
+    root1->left->left = new TreeNode(1);
+    root1->left->right = new TreeNode(3);
+    Solution sol;
+    cout << sol.checkTree(root1);
+}   
