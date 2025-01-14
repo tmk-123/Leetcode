feadf045f4b335aa52cd687d1acdcc96ee057d04
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-14 18:00:53 +0700

    Last Stone Weight

--- 1046.cpp
+++ 1046.cpp
@@ -1,21 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        // Tạo max-heap bằng cách dùng priority_queue
-        priority_queue<int> maxHeap(stones.begin(), stones.end());
+        priority_queue<int> pq(stones.begin(), stones.end());
 
-        // Lặp lại cho đến khi còn 1 hoặc không còn viên đá nào
-        while (maxHeap.size() > 1) {
-            int stone1 = maxHeap.top(); maxHeap.pop(); // Lấy viên lớn nhất
-            int stone2 = maxHeap.top(); maxHeap.pop(); // Lấy viên lớn thứ hai
+        while (pq.size() > 1) {
+            int stone1 = pq.top();
+            pq.pop();
+            int stone2 = pq.top();
+            pq.pop();
 
-            // Nếu hai viên không bằng nhau, đưa phần dư trở lại heap
             if (stone1 != stone2) {
-                maxHeap.push(stone1 - stone2);
+                pq.push(stone1 - stone2);
             }
         }
 
-        // Nếu còn một viên, trả về khối lượng của nó, nếu không thì trả về 0
-        return maxHeap.empty() ? 0 : maxHeap.top();
+        if (pq.empty()) return 0;
+        return pq.top();
     }
 };
+
+int main() {
+    vector<int> v = {2, 7, 4, 1, 8, 1};
+    Solution sol;
+    cout << sol.lastStoneWeight(v);
+}
