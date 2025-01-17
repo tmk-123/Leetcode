68f8165555df718394cf412217e01d79047ca5fb
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-18 00:14:24 +0700

    Final Array State After K Multiplication Operations I

--- 3264.cpp
+++ 3264.cpp
@@ -5,22 +5,36 @@ using namespace std;
 struct Element {
     int value, index;
 
-    bool operator>(Element other) {
+    // phải có const ở tham số và ngoài để hàm cố định, tham số cố định
+    bool operator>(const Element& other) const {
+        if (value == other.value) return index > other.index;
         return value > other.value; // sap xep giam dan, greater se dao nguoc thanh tang dan
     }
-}
+};
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        priority_queue<int, vector<int>, greater<int>> minHeap(nums.begin(), nums.end());
+        priority_queue<Element, vector<Element>, greater<Element>> minHeap;
+        for (int i = 0; i < nums.size(); i++) minHeap.push({nums[i], i});
+
         while (k--) {
-            
+            Element minElement = minHeap.top();
+            minHeap.pop();
+
+            nums[minElement.index] *= multiplier;
+
+            minHeap.push({nums[minElement.index], minElement.index});
         }
+
+        return nums;
     }
 };
   
 int main() {
-    vector<int> v = {2, 1, 3, 5, 6};
+    vector<int> v = {1, 3, 5};
     Solution sol;
-    vector<int> res = sol.getFinalState(v, 5, 2);
+    vector<int> res = sol.getFinalState(v, 5, 3);
+    for (int x : res) {
+        cout << x << " ";
+    }
 }
