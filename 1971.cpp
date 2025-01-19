d65599dbd19a3c25e7cfe7d6433d03ed52e022b6
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-01-19 11:47:05 +0700

    Find if Path Exists in Graph

--- 1971.cpp
+++ 1971.cpp
@@ -4,16 +4,6 @@ using namespace std;
 
 class Solution {
 public:
-    bool dfs(int current, int destination, unordered_map<int, vector<int>> graph, unordered_set<int>& visited) {
-        if (current == destination) return true;
-        visited.insert(current);
-        for (int neighbor : graph[current]) {
-            if (visited.find(neighbor) == visited.end()) {
-                if (dfs(neighbor, destination, graph, visited)) return true;
-            }
-        }
-        return false;
-    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         unordered_map<int, vector<int>> graph;
         for (auto edge : edges) {
@@ -21,8 +11,24 @@ public:
             graph[edge[1]].push_back(edge[0]);
         }
 
+        queue<int> q;
         unordered_set<int> visited;
-        return dfs(source, destination, graph, visited);
+        q.push(source);
+        visited.insert(source);
+
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+            if (current == destination) return true;
+            for (int neighbor : graph[current]) {
+                if (visited.find(neighbor) == visited.end()) {
+                    visited.insert(neighbor);
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        return false;
     }
 };
 
