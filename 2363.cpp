2754925538b3316a971473eaf767148be5e8591c
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-20 22:42:04 +0700

    Merge Similar Items

---
+++ 2363.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool compare(vector<int> a, vector<int> b) {
+    return a[0] < b[0];
+}
+
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        map<int, int> m;
+        for (vector<int> x : items1) {
+            m[x[0]] += x[1];
+        }
+        for (vector<int> x : items2) {
+            m[x[0]] += x[1];
+        }
+
+        vector<vector<int>> res;
+        for (pair<int, int> x : m) {
+            res.push_back({x.first, x.second});
+        }
+        return res;
+    }
+};
+
+int main() {
+    vector<vector<int>> a = {{1, 1}, {4, 5}, {3, 8}};
+    vector<vector<int>> b = {{3, 1}, {1, 5}};
+
+    Solution sol;
+    vector<vector<int>> res = sol.mergeSimilarItems(a, b);
+    for (auto x : res) {
+        for (int y : x) {
+            cout << y << " ";
+        } 
+        cout << endl;
+    } 
+}
