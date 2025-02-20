8cce597226b423627efded3a685f9d172b00c5c5
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-21 01:02:12 +0700

    Number of Unequal Triplets in Array

--- 2475.cpp
+++ 2475.cpp
@@ -1,11 +1,26 @@
 #include<bits/stdc++.h>
 
-int unequalTriplets(vector<int>& A) {
-    int trips = 0, pairs = 0, count[1001] = {};
-    for (int i = 0; i < A.size(); ++i) {
-        trips += pairs - count[A[i]] * (i - count[A[i]]);
-        pairs += i - count[A[i]];
-        count[A[i]] += 1;
-    }
-    return trips;
+using namespace std;
+
+class Solution {
+    public:
+        int unequalTriplets(vector<int>& nums) {
+            int c = 0;
+            for (int i = 0; i < nums.size(); i++) {
+                for (int j = i; j < nums.size(); j++) {
+                    for (int k = j; k < nums.size(); k++) {
+                        if(nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]){
+                            c++;
+                        }
+                    }
+                }
+            }
+            return c;
+        }
+    };
+
+int main() {
+    vector<int> v = {4,4,2,4,3};
+    Solution sol;
+    cout << sol.unequalTriplets(v);
 }
