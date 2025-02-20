60d21a9b86a0ef754d738f1512f26b2dbe3fa909
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-20 23:37:09 +0700

    Number of Unequal Triplets in Array

---
+++ 2475.cpp
@@ -0,0 +1,9 @@
+int unequalTriplets(vector<int>& A) {
+    int trips = 0, pairs = 0, count[1001] = {};
+    for (int i = 0; i < A.size(); ++i) {
+        trips += pairs - count[A[i]] * (i - count[A[i]]);
+        pairs += i - count[A[i]];
+        count[A[i]] += 1;
+    }
+    return trips;
+}
