39e301178866a6df64be543b6dd75f0bcb261341
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-22 00:32:56 +0700

    Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ 2996.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int countSum = nums[0];
+        for(int i =1; i<nums.size();i++){
+            if(nums[i-1]+1 == nums[i]) countSum += nums[i];
+            else break;
+        }
+        sort(nums.begin(),nums.end());
+        for(int i = 0; i< nums.size(); i++){
+            if(countSum == nums[i]){
+                countSum++;
+            }
+        }
+        return countSum;
+        
+    }
+};
