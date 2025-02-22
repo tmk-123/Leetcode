4b4c8f5afd12b237d926b0b495f06d165d1d2a1e
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-22 14:48:47 +0700

    Minimum Average of Smallest and Largest Elements Solved Easy Topics Companies Hin

---
+++ 3194.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    double minimumAverage(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int i = 0, j = nums.size() - 1;
+        double minAverage = 1e9;
+        while (i < j) {
+            minAverage = min(minAverage, (nums[j--] + nums[i++]) / 2.0);
+        }
+
+        return minAverage;
+    }
+};
