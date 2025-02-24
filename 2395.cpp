82fa17d9a2bce53239c9103490ebf533f474e2ab
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-24 18:55:35 +0700

    Find Subarrays With Equal Sum

---
+++ 2395.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+bool findEqualSumSubarrays(vector<int>& nums) {
+    unordered_set<int> seenSums;
+    for (int i = 0; i < nums.size() - 1; i++) {
+        int sum = nums[i] + nums[i + 1];  // Tính tổng của subarray có độ dài 2
+        if (seenSums.count(sum)) return true;  // Nếu tổng đã xuất hiện, trả về true
+        seenSums.insert(sum);  // Lưu tổng vào set
+    }
+    return false;  // Nếu duyệt hết không tìm thấy, trả về false
+}
+};
