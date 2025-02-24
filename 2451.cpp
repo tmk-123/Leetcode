5e4400ebeb170911e34e3e3451b51e92912acaab
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-24 23:44:59 +0700

    Odd String Difference

--- 2451.cpp
+++ 2451.cpp
@@ -3,24 +3,21 @@
 using namespace std;
 
 class Solution {
-public:
-    string oddString(vector<string>& words) {
-        map<vector<int>, int> m;
-        map<vector<int>, string> S;
-        for (string s : words) {
-            vector<int> v;
-            for (int i = 1; i < s.length(); i++) {
-                v.push_back(s[i] - s[i - 1]);
+    public:
+        string oddString(vector<string>& words) {
+            map<vector<int>,vector<string>> mp;
+            for(auto c:words){
+                vector<int> temp;
+                for(int i=1;i<c.size();++i){
+                    temp.push_back(c[i]-c[i-1]);
+                }
+                mp[temp].push_back(c);
             }
-            m[v]++;
-            S[v] = s;
-        }
+            
+            for(auto [f,s]:mp){
+                if(s.size()==1)return s[0];
+            }
 
-        for (auto x : m) {
-            if (x.second == 1) {
-                return S[x.first];
-            }
+            return "";
         }
-        return "";
-    }
-};
+    };
