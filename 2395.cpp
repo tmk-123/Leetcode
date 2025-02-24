62754aaadbff04f3d37712c873af70a29a5a2adf
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-24 22:29:48 +0700

    Find Subarrays With Equal Sum

--- 2395.cpp
+++ 2395.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    bool findEqualSumSubarrays(vector<int>& nums) {
+    bool findSubarrays(vector<int>& nums) {
         unordered_set<int> seenSums;
         for (int i = 0; i < nums.size() - 1; i++) {
             int sum = nums[i] + nums[i + 1];  // Tính tổng của subarray có độ dài 2
