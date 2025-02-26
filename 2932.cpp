948785de9548748fe81d381627581fc18d94ada8
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-26 12:33:06 +0700

    v

---
+++ 2932.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int maxXor = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size(); j++) {
+                int x = nums[i], y = nums[j];
+                if (abs(x - y) <= min(x, y)) {
+                    maxXor = max(maxXor, x ^ y);
+                }
+            }
+        }
+        return maxXor;
+    }
+};
+    
