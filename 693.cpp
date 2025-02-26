f0f2956ef2262bbaf5fd34b219c1026bccf324b5
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-26 16:05:35 +0700

    Binary Number with Alternating Bits

--- 693.cpp
+++ 693.cpp
@@ -5,19 +5,12 @@ using namespace std;
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        int lastBit = n & 1;
-        int i = 1;
-        while ((1 << i) <= n) {
-            int bit = 0;
-            if ((n & (1 << i++)) > 0) bit = 1;
-            if (bit == lastBit) return false;
-            lastBit = bit;
-        }
-        return true;
+        long long x = n ^ (n >> 1); // nếu n có dạng 101010... thì sẽ full 1;
+        return (x & (x + 1)) == 0;
     }
 };
 
 int main() {
     Solution sol;
-    cout << sol.hasAlternatingBits(5);
+    cout << sol.hasAlternatingBits(4);
 }
