274c4255a589a46c7242c5d65d7457c82df445dd
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-27 06:28:55 +0700

    Number of Steps to Reduce a Number to Zero

---
+++ 1342.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int cnt = 0;
+        while (num > 0) {
+            if (num % 2 == 0) num /= 2;
+            else num -= 1;
+            cnt++;
+        }
+        return cnt;
+    }
+};
