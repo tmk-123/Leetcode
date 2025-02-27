c99b89a596ae4891253308ab65dbf87c7d2376f4
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-27 22:19:50 +0700

    Jewels and Stones

---
+++ 771.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        unordered_set<char> jewelSet(jewels.begin(), jewels.end());
+        int count = 0;
+        for (char c : stones) {
+            if (jewelSet.count(c)) { 
+                count++;
+            }
+        }
+        return count;
+    }
+};
