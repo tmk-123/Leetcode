2f82aa8cd5344925b6901ed2896cdadc4f5ab00a
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-27 08:56:32 +0700

    Distribute Candies

---
+++ 575.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        set<int> Set(candyType.begin(), candyType.end());
+        if (Set.size() < candyType.size() / 2) return Set.size();
+        return candyType.size() / 2;
+    }
+};
