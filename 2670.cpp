e07997295801404828ba510e46341b9347512a50
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-27 20:35:35 +0700

    Find the Distinct Difference Array

--- 2670.cpp
+++ 2670.cpp
@@ -5,18 +5,24 @@ using namespace std;
 class Solution {
 public:
     vector<int> distinctDifferenceArray(vector<int>& nums) {
-        unordered_map<int, int> m;
-        for (int n : nums) m[n]++;
+        int n = nums.size();
+        vector<int> diff(n);
+        
+        unordered_set<int> prefix, suffix;
+        vector<int> suffix_count(n + 1, 0);
 
-        unordered_set<int> s;
-        vector<int> res;
-        for (int n : nums) {
-            s.insert(n);
-            m[n]--;
-            if (m[n] == 0) m.erase(n);
-
-            res.push_back(s.size() - m.size());
+        // Tính số lượng phần tử khác nhau trong suffix
+        for (int i = n - 1; i >= 0; i--) {
+            suffix.insert(nums[i]);
+            suffix_count[i] = suffix.size();
         }
-        return res;
+
+        // Tính diff[i]
+        for (int i = 0; i < n; i++) {
+            prefix.insert(nums[i]);
+            diff[i] = prefix.size() - suffix_count[i + 1];
+        }
+
+        return diff;
     }
-};  
+};
