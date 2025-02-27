a75d9789319e06210e859bc8b5d880496d1f4750
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-02-27 08:59:00 +0700

    Distribute Candies

--- 575.cpp
+++ 575.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        set<int> Set(candyType.begin(), candyType.end());
+        unordered_set<int> Set(candyType.begin(), candyType.end());
         if (Set.size() < candyType.size() / 2) return Set.size();
         return candyType.size() / 2;
     }
