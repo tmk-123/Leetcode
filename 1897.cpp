b288efa2b6f0fb764bec3c4171114ef073e00764
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-02 20:47:53 +0700

    Redistribute Characters to Make All Strings Equal Solved Easy Topics Companies Hint

---
+++ 1897.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        unordered_map<char, int> freq;
+        int n = words.size();
+
+        for (string& word : words) {
+            for (char c : word) {
+                freq[c]++;
+            }
+        }
+
+        for (auto& [ch, count] : freq) {
+            if (count % n != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
