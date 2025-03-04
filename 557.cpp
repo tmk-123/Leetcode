db5097e711cfbec835fcecf70b625914e8befbea
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-04 22:36:28 +0700

    Reverse Words in a String III

---
+++ 557.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        istringstream iss(s);
+        vector<std::string> words;
+        string word;
+        while (iss >> word) {
+            reverse(word.begin(), word.end());
+            words.push_back(word);
+        }
+        
+        string result = "";
+        for (const string& reversedWord : words) {
+            result += reversedWord + " ";
+        }
+        result.pop_back(); 
+        
+        return result;        
+    }
+};
