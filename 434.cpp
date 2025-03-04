efbf378535db3aebcb8056f074a4a25f91e8764c
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-04 19:43:08 +0700

    Number of Segments in a String S

---
+++ 434.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSegments(string s) {
+        int cnt = 0;
+        stringstream ss(s);
+        string word;
+        while (ss >> word) {
+            cnt++;
+        }
+        return cnt;
+    }
+};
