b8c0e66671669ca3f5754af8444ab4b9ad486168
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-05 13:36:36 +0700

    Reverse Only Letters

---
+++ 917.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (isalpha(s[i]) && isalpha(s[j])) {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+            else if (!isalpha(s[i])) i++;
+            else j--;
+        }
+        return s;
+    }
+};
