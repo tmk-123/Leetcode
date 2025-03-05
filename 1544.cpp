1fc55b157356a69571529d9759c8d32b4a497885
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-05 22:22:28 +0700

    Make The String Great

---
+++ 1544.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+        
+        for (char c : s) {
+            if (!st.empty() && abs(st.top() - c) == 32) { // 'a' - 'A' = 32
+                st.pop(); 
+            } else {
+                st.push(c);
+            }
+        }
+        
+        string result;
+        while (!st.empty()) {
+            result = result + st.top();
+            st.pop();
+        }
+        
+        return result;
+    }
+};
