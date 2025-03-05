5dd10d79a165c89ad1911d65585216fb2a46c3bf
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-05 15:08:33 +0700

    Greatest Common Divisor of Strings

---
+++ 1071.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return ""; // Nếu không có mẫu chung
+        int gcd_len = gcd(str1.size(), str2.size());
+        return str1.substr(0, gcd_len);
+    }
+};
