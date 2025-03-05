0f1d36e77fa007a4ee4cb29ca35c7d228bc1a325
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-05 13:44:37 +0700

    Long Pressed Name

---
+++ 925.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+bool isLongPressedName(string name, string typed) {
+    int i = 0, j = 0;
+    
+    while (j < typed.length()) {
+        if (i < name.length() && name[i] == typed[j]) {
+            i++; 
+            j++; 
+        } 
+        else if (j > 0 && typed[j] == typed[j - 1]) j++; 
+        else return false; 
+    }
+    
+    return i == name.length(); // Đảm bảo đã duyệt hết name
+}
+
+};
