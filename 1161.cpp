1e013fe162c7153dfdc80c5d5603131145d57d17
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-08 23:44:20 +0700

    Maximum Level Sum of a Binary Tree

---
+++ 1161.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 1, maxSum = INT_MIN;
+        int maxDepth = -1;
+
+        while (!q.empty()) {
+            int size = q.size();
+            int sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                sum += node->val;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxDepth = depth;
+            }
+            depth++;
+        }
+        return maxDepth;
+    }
+};
