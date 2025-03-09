315a748e4f2f115c055c34031d43833b8d928340
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-09 21:05:27 +0700

    Cousins in Binary Tree II

---
+++ 2641.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        root->val = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+            vector<TreeNode*> v;
+            int sum = 0;
+
+            while (n--) {
+                TreeNode* node = q.front(); q.pop();
+                v.push_back(node);
+                if (node->left) {
+                    q.push(node->left);
+                    sum += node->left->val;
+                }
+                if (node->right) {
+                    q.push(node->right);
+                    sum += node->right->val;
+                }
+            }
+
+            for (auto& node : v) {
+                int t = sum;
+                if (node->left) t -= node->left->val;
+                if (node->right) t -= node->right->val;
+                if (node->left) node->left->val = t;
+                if (node->right) node->right->val = t; 
+            }
+        }
+
+        return root;
+    }
+};
