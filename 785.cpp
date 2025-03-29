049f0073493f0a26541e465f2ea276b012e7db5c
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-03-29 17:41:25 +0700

    Is Graph Bipartite?

---
+++ 785.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> color(n, -1);
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) color[i] = 0;
+            q.push(i);
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for (auto& v : graph[u]) {
+                    if (color[v] == color[u]) return false;
+                    else if (color[v] == -1) {
+                        color[v] = 1 - color[u];
+                        q.push(v);
+                    }
+                }
+            }
+        }        
+
+        return true;
+    }
+};
