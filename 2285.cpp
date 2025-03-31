936fd0974611bb306f21369661d7ebe042fb6029
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-04-01 00:47:57 +0700

    Maximum Total Importance of Roads

---
+++ 2285.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<int> degree(n, 0);
+
+        for (auto& road : roads) {
+            degree[road[0]]++;
+            degree[road[1]]++;
+        }
+
+        vector<int> cities(n);
+        for (int i = 0; i < n; i++) cities[i] = i;
+        sort(cities.begin(), cities.end(), [&](int a, int b) {
+            return degree[a] > degree[b];
+        });
+
+        long long res = 0;
+        for (int i = 0; i < n; i++) {
+            res += (long long) (n - i) * degree[cities[i]];
+        }
+        return res;
+    }
+};
