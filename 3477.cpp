4801a880e981abe10caf7d7f9858ff27786a34bb
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-04-16 19:34:15 +0700

    Fruits Into Baskets II

--- 3477.cpp
+++ 3477.cpp
@@ -3,66 +3,53 @@
 using namespace std;
 
 class Solution {
+    vector<int> tree;
+    int n;
+
+    void build(vector<int>& baskets, int node, int l, int r) {
+        if (l == r) tree[node] = baskets[l];
+        else {
+            int mid = (l + r) / 2;
+            build(baskets, 2 * node, l, mid);
+            build(baskets, 2 * node + 1, mid + 1, r);
+            tree[node] = max(tree[2 * node], tree[2 * node + 1]);
+        }
+    }
+
+    void update(int node, int l, int r, int idx) {
+        if (l == r) {
+            tree[node] = -1;
+        } else {
+            int mid = (l + r) / 2;
+            if (idx <= mid) update(2 * node, l, mid, idx);
+            else update(2 * node + 1, mid + 1, r, idx);
+            tree[node] = max(tree[2 * node], tree[2 * node + 1]);
+        }
+    }
+
+    int query(int node, int l, int r, int fruit) {
+        if (tree[node] < fruit) return -1;
+        if (l == r) return l;
+        int mid = (l + r) / 2;
+        if (tree[2 * node] >= fruit)
+            return query(2 * node, l, mid, fruit);
+        else
+            return query(2 * node + 1, mid + 1, r, fruit);
+    }
+
 public:
-    struct SegmentTree {
-        int n;
-        vector<int> tree;
-
-        SegmentTree(const vector<int>& a) {
-            n = a.size();
-            tree.assign(4 * n, 0);
-            build(1, 0, n - 1, a);
-        }
-
-        void build(int id, int l, int r, const vector<int>& a) {
-            if (l == r) {
-                tree[id] = a[l];
-                return;
-            }
-            int mid = (l + r) / 2;
-            build(id * 2, l, mid, a);
-            build(id * 2 + 1, mid + 1, r, a);
-            tree[id] = tree[id * 2] + tree[id * 2 + 1];
-        }
-
-        void update(int id, int l, int r, int pos) {
-            if (l == r) {
-                tree[id] = -1;
-                return;
-            } 
-            int mid = (l + r) / 2;
-            if (pos <= mid) update(id * 2, l, mid, pos);
-            else update(id * 2 + 1, mid + 1, r, pos);
-            tree[id] = max(tree[id], tree[id * 2 + 1]);
-        }
-
-        int query(int id, int l, int r, int val) {
-            if (tree[id] < val) return -1;
-            if (l == r) return l;
-            int mid = (l + r) / 2;
-            int res = query(id * 2, l, mid, val);
-            if (res == -1) res = query(id * 2 + 1, mid + 1, r, val);
-            return res;
-        }
-
-        int find_first(int val) {
-            return query(1, 0, n - 1, val);
-        }
-        void mark_used(int pos) {
-            update(1, 0, n - 1, pos);
-        }
-    };
-
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        int n = fruits.size();
-        SegmentTree st(baskets);
-        int unplaced = 0;
+        n = baskets.size();
+        tree.resize(4 * n);
+        build(baskets, 1, 0, n - 1);
 
-        for (int i = 0; i < n; i++) {
-            int pos = st.find_first(fruits[i]);
-            if (pos == -1) unplaced++;
-            else st.mark_used(pos);
+        int unplaced = 0;
+        for (int fruit : fruits) {
+            int idx = query(1, 0, n - 1, fruit);
+            if (idx == -1) unplaced++;
+            else update(1, 0, n - 1, idx);
         }
+
         return unplaced;
     }
 };
