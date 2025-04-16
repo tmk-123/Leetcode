f109bd56c70cc2d119f538ff8e7a089daac00920
Tran Manh Khiem <khiemmanhtran@gmail.com>
2025-04-16 21:52:04 +0700

    My Calendar I

---
+++ 729.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class MyCalendar {
+    vector<pair<int, int>> books;
+public:    
+    bool book(int start, int end) {
+        for (pair<int, int> p : books)
+            if (max(p.first, start) < min(end, p.second)) return false;
+        books.push_back({start, end});
+        return true;
+    }
+};
